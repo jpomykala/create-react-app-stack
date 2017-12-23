67878009ca4fef60d5607811b749b5dd2f33e344
evelan <evelan3094@gmail.com>
2017-12-23 02:03:39 +0100

    add gitignore & format

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 #!/usr/bin/env node
 let shell = require('shelljs');
 let colors = require('colors');
-let fs = require('fs'); //fs already comes included with node.
+let fs = require('fs');
+let packageJson = require('./package.json');
 
-let appName = process.argv[2]
-let appDirectory = `${process.cwd()}/${appName}`
+let appName = process.argv[2];
+let appDirectory = `${process.cwd()}/${appName}`;
+const additionalPackages = ["redux", "react-router", "react-redux", "redux-thunk", "react-router-dom", "bootstrap",
+  "reactstrap", "moment", "classnames", "axios"];
 
 const createReactApp = () => {
-  return new Promise(resolve=>{
-    if(appName){
+  return new Promise(resolve => {
+    if (appName) {
       shell.exec(`create-react-app ${appName}`, () => {
-        console.log("Created react app")
-      resolve(true)
-    })
-    }else{
-      console.log("\nNo app name was provided.".red)
-    console.log("\nProvide an app name in the following format: ")
-  console.log("\ncreate-react-redux-router-app ", "app-name\n".cyan)
-  resolve(false)
-}
-})
-}
+        console.log("Basic react app created");
+        resolve(true)
+      })
+    } else {
+      console.log("\nNo app name was provided.".red);
+      console.log("\nProvide an app name in the following format: ");
+      console.log(`\n${packageJson.name} `, "app-name\n".cyan);
+      resolve(false);
+    }
+  })
+};
 
-const cdIntoNewApp = () => {
-  return new Promise(resolve=>{
-    shell.exec(`cd ${appName}`, ()=>{resolve()})
-})
-}
+const goToCreatedDirectory = () => {
+  return new Promise(resolve => shell.exec(`cd ${appName}`, () => resolve()));
+};
 
-const installPackages = () => {
-  return new Promise(resolve=>{
-    console.log("\nInstalling redux, react-router, react-router-dom, react-redux, and redux-thunk\n".cyan)
-  shell.exec(`npm install --save redux react-router react-redux redux-thunk react-router-dom bootstrap reactrap moment classnames axios`, () => {
-    console.log("\nFinished installing packages\n".green)
-  resolve()
-})
-})
-}
+const installAdditionalPackages = () => {
+  return new Promise(resolve => {
+    console.log("\nInstalling essential packages\n".cyan);
+    shell.exec(`npm install --save ${additionalPackages.join(' ')}`,
+        () => {
+          console.log("\nFinished installing packages\n".green);
+          resolve()
+        })
+  })
+};
+
+const updateTemplates = () => {
+  return new Promise(resolve => {
+    console.log("\nNo templates to update");
+    resolve();
+  });
+};
 
 const run = async () => {
-  let success = await createReactApp()
-  if(!success){
-    console.log('Something went wrong while trying to create a new React app using create-react-app'.red)
+  let success = await createReactApp();
+  if (!success) {
+    console.log('Something went wrong while trying to create a new React app using create-react-app'.red);
     return false;
   }
-  await cdIntoNewApp()
-  await installPackages()
-  await updateTemplates()
-  console.log("All done")
-}
-run()
+  await goToCreatedDirectory();
+  await installAdditionalPackages();
+  await updateTemplates();
+};
+
+run().then(() => console.log("All done"));
